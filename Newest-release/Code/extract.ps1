899b605aba1377a039146b7b828a0d649d36f3df
Mr-Unknown-74 <77338053+Mr-Unknown-74@users.noreply.github.com>
2022-12-05 13:26:23 -0500

    Add files via upload

---
+++ Newest-release/Code/extract.ps1
@@ -0,0 +1,13 @@
+$drvlet = Get-ChildItem function:[d-z]: -n | Where-Object{ !(test-path $_) } | Select-Object -First 1
+$path = (Get-item $PSScriptRoot ).parent.FullName
+$isoImgFolder = "$path\Copy the ISO file here\"
+$isoImg = Get-ChildItem $isoImgFolder -Name -Recurse -Include *.iso | Select-Object -First 1
+$isoImgPath = "$isoImgFolder$isoImg"
+$IsoFiles = "$path\Code\ISO"
+$diskImg = Mount-DiskImage -ImagePath "$isoImgPath"  -NoDriveLetter
+$volInfo = $diskImg | Get-Volume
+mountvol $drvlet $volInfo.UniqueId
+Write-Output ("Extracting files... This may take a while.")
+cmd.exe /c Xcopy "$drvlet\" "$IsoFiles" /E /F /Q
+Start-Process "$path\Code\Install.cmd"
+DisMount-DiskImage -ImagePath $isoImgPath
